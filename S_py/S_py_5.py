f6a927bb3b2dc2e2def6820f93a6525cd845350c
WabWab-E <HBbang03@gmail.com>
2021-04-01 14:11:25 +0900

    Update various folders

--- S_py/S_py_5.py
+++ S_py/S_py_5.py
@@ -13,10 +13,14 @@ print("-" * 50)
 
 # [format] 을 이용한 문자열 형식화
 
-a, b = map(int, input("덧셈할 두 수를 입력 : ").split(" "))
-c = "-" * 13
+firstNum, secondNum = map(int, input("덧셈할 두 수를 입력 : ").split(" "))
+line = "-" * 13
 
-print("{0:>10}\n{1:<5}{2:>5}\n{3}\n{4:10}".format(a, "+", b, c, a + b))
+print(
+    "{0:>10}\n{1:<5}{2:>5}\n{3}\n{4:10}".format(
+        firstNum, "+", secondNum, line, firstNum + secondNum
+    )
+)
 
 print("-" * 50)
 
@@ -30,8 +34,7 @@ def twotimes(x):
     return y
 
 
-d = twotimes(2)
-print(d)
+print(twotimes(2))
 
 print("-" * 50)
 
@@ -52,9 +55,9 @@ def oddOreven(x):
         return odd
 
 
-inp = int(input("홀짝 : "))
+num = int(input("홀짝 : "))
 
-print(oddOreven(inp))
+print(oddOreven(num))
 
 print("-" * 50)
 
@@ -76,8 +79,61 @@ def LY_NY(x):
         return normal_YR
 
 
-inp = int(input("윤년? 평년? : "))
+ly_Year = int(input("윤년? 평년? : "))
 
-print(LY_NY(inp))
+print(LY_NY(ly_Year))
 
 print("-" * 50)
+
+## 3 ##
+
+thirty = [4, 6, 9, 11]
+thirtyOne = [1, 3, 5, 7, 8, 10, 12]
+
+
+def days1(month):
+    if month in thirty:
+        return 30
+    elif month in thirtyOne:
+        return 31
+    else:
+        return 28
+
+
+print("-" * 50)
+
+## 4 ##
+
+thirty = [4, 6, 9, 11]
+thirtyOne = [1, 3, 5, 7, 8, 10, 12]
+
+
+def days2(x, y):
+    is_Leap_year = False
+
+    if x % 4 == 0:
+        is_Leap_year = True
+    elif x % 100 == 0:
+        is_Leap_year = True
+        if x % 400 == 0:
+            is_Leap_year = True
+
+    if y in thirty:
+        return 30
+    elif y in thirtyOne:
+        return 31
+    else:
+        if is_Leap_year:
+            return 29
+        else:
+            return 28
+
+
+year, month = map(int, input("년 월 : ").split())
+
+print(days2(year, month))
+
+print("-" * 50)
+
+## 람다 함수 (익명함수) [lambda] - lambda 매개변수 : 리턴값
+# 람다 표현식 자체 호출 시 - (lambda 매개변수 : 리턴값) (인수)
