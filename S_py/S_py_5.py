db5149e2c48db9db91eb5d8450109d93b683d730
WabWab-E <HBbang03@gmail.com>
2021-04-05 15:52:27 +0900

    Update S_py_5.py

--- S_py/S_py_5.py
+++ S_py/S_py_5.py
@@ -137,3 +137,22 @@ print("-" * 50)
 
 ## 람다 함수 (익명함수) [lambda] - lambda 매개변수 : 리턴값
 # 람다 표현식 자체 호출 시 - (lambda 매개변수 : 리턴값) (인수)
+
+# def
+def plus_ten(x):
+    return x + 10
+
+
+print(list(map(plus_ten, [1, 2, 3])))
+
+# lambda
+print(list(map(lambda x: x + 10, [1, 2, 3])))
+
+# lambda + input
+print(list(map(lambda x: x + 10, map(int, input("수 입력 : ").split()))))
+
+# 람다 함수에는 조건식도 넣을 수 있다
+
+oneToTen = list(range(1, 11))
+
+print(list(map(lambda x: str(x) if x % 2 == 0 else x, oneToTen)))
