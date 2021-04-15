e83ea330d8821e30726bf6db1281a9090f397d82
WabWab-E <HBbang03@gmail.com>
2021-04-15 13:51:25 +0900

    Update S_py_C_21

--- S_py_C/S_py_C_21.py
+++ S_py_C/S_py_C_21.py
@@ -9,27 +9,27 @@ def diffSum(x, y, z):
     numSum = x + y + z
     squareSum = (x ** 2) + (y ** 2) + (z ** 2)
 
-    rv = abs(numSum - squareSum)
-
-    diffSum2(rv)
-    return rv
+    return abs(numSum - squareSum)
 
 
 def diffSum2(x):
+    # global이 없다면 diffSum2 내부의 'm'값만 변환되며 함수 밖에 있는 'm'에는 전혀 영향이 없다
     global m
 
     if x > m:
         m = x
 
-    print("현재 수 : ", x, end=" ")
+    print("현재 수 : ", x, end=" | ")
 
 
 while True:
     xx, yy, zz = map(int, input("3개의 수 입력 : ").split(" "))
 
-    if xx + yy + zz == 0:
+    # 3개의 값 모두 0을 입력하면 프로그램 종료
+    if xx == yy == zz == 0:
         print("종료")
         break
-    diffSum(xx, yy, zz)
+
+    diffSum2(diffSum(xx, yy, zz))
 
     print("가장 컸던 수 : ", m)
